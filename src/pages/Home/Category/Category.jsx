d254d46c2c3d127b43daa10eab444e07a9cb8d9d
rejaul rasel <rejaulhasanrasel28@gmail.com>
2023-09-16 14:45:40 +0600

    first commit

---
+++ src/pages/Home/Category/Category.jsx
@@ -0,0 +1,46 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
+import slide1 from '../../../assets/home/slide1.jpg'
+import slide2 from '../../../assets/home/slide2.jpg'
+import slide3 from '../../../assets/home/slide3.jpg'
+import slide4 from '../../../assets/home/slide4.jpg'
+
+const Category = () => {
+    return (
+        <>
+            <Swiper
+                slidesPerView={5}
+                spaceBetween={30}
+                centeredSlides={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper mb-24"
+            >
+                <SwiperSlide>
+                    <img src={slide1} alt="" />
+                    <h1 className='text-2xl lg:text-4xl text-center text-white -mt-16'>Salad</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide2} alt="" />
+                    <h1 className='text-2xl lg:text-4xl text-center text-white -mt-16'>Pizza</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide3} alt="" />
+                    <h1 className='text-2xl lg:text-4xl text-center text-white -mt-16'>Pasta</h1>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide4} alt="" />
+                    <h1 className='text-2xl lg:text-4xl text-center text-white -mt-16'>Cake</h1>
+                </SwiperSlide>
+
+
+            </Swiper>
+        </>
+    );
+};
+
+export default Category;
