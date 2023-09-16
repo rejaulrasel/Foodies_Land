ac2abb0033212d36dfead5e570e837030f12ebea
rejaul rasel <rejaulhasanrasel28@gmail.com>
2023-09-16 16:30:18 +0600

    added banner,slider and sectionTitle component

---
+++ src/components/SectionTitle/SectionTitle.jsx
@@ -0,0 +1,10 @@
+const SectionTitle = ({ heading, subHeading }) => {
+    return (
+        <div className="w-3/12 mx-auto text-center mb-8 ">
+            <p className="text-yellow-600 mb-2">--- {subHeading} ---</p>
+            <h3 className="text-3xl border-y-4 py-2">{heading}</h3>
+        </div>
+    );
+};
+
+export default SectionTitle;
--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -6,10 +6,14 @@ import slide1 from '../../../assets/home/slide1.jpg'
 import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 const Category = () => {
     return (
         <>
+        <SectionTitle
+        subHeading='From 11:00am to 10:00pm' heading='ORDER ONLINE'
+        ></SectionTitle>
             <Swiper
                 slidesPerView={5}
                 spaceBetween={30}
